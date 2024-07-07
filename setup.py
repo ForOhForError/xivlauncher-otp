82ada189b422e47560b5655e197fbf261692eb3f
ForOhForError <16399039+ForOhForError@users.noreply.github.com>
2024-07-07 16:44:16 -0400

    updated with some clipboard stuff

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      packages=setuptools.find_packages(),
-     install_requires=['pyotp'],
+     install_requires=['pyotp', 'pyperclip'],
      classifiers=[
          "Programming Language :: Python :: 3",
          "Operating System :: OS Independent",
--- xivlauncher_otp.py
+++ xivlauncher_otp.py
@@ -3,20 +3,38 @@ import os
 import urllib.request
 from datetime import datetime, timedelta
 from time import sleep
+import sys
+import argparse
 
 FUTURE_LOOKAHEAD_SECONDS = 5
+XIV_OTP_SECRET_VAR = "XIV_OTP_SECRET"
 
-def main(args=[]):
+def copy_to_clip(code:str):
+	import pyperclip
+	pyperclip.copy(code)
+
+def get_otp_secret_from_env():
+	if XIV_OTP_SECRET_VAR not in os.environ:
+		return ""
+	else:
+		return os.getenv(XIV_OTP_SECRET_VAR)
+
+def main():
 	"""
 	Read TOTP secret from the environment, generate the current login code,
 	and send it to XIV Launcher via GETing its local login page.
 	"""
-	if 'XIV_OTP_SECRET' not in os.environ:
-		print("XIV_OTP_SECRET not found as environment variable; exiting")
-		return 1
-	secret = os.environ['XIV_OTP_SECRET']
+	parser = argparse.ArgumentParser(
+		prog='xivlauncher-otp',
+		description='Automatically sends an OTP to XIVLauncher'
+	)
+	parser.add_argument('-s', '--secret', type=str, default=get_otp_secret_from_env(), help=f'Your TOTP Secret. If not given, uses {XIV_OTP_SECRET_VAR} instead.')
+	parser.add_argument('-C', '--copy-to-clip', action='store_true', help='If set, also attempts to copy the OTP to the clipboard')
+	parsed = parser.parse_args()
+
+	secret = parsed.secret
 	if not secret:
-		print("XIV_OTP_SECRET not defined; exiting")
+		print(f"TOTP Secret not supplied; either pass using `--secret YOURSECRETHERE` or setting the env var {XIV_OTP_SECRET_VAR}")
 		return 1
 	auth = TOTP(secret)
 	time_now = datetime.now()
@@ -29,6 +47,10 @@ def main(args=[]):
 	if(otp_now != otp_future):
 		otp = otp_future
 		sleep(FUTURE_LOOKAHEAD_SECONDS)
+
+	if parsed.copy_to_clip:
+		copy_to_clip(otp)
+
 	auth_url = "http://localhost:4646/ffxivlauncher/{}".format(otp)
 	with urllib.request.urlopen(auth_url) as connect:
 		pass
@@ -36,6 +58,5 @@ def main(args=[]):
 	return 0
 
 if __name__ == '__main__':
-	import sys
-	sys.exit(main(sys.argv))
+	sys.exit(main())
 
